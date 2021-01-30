8e1e5c5d48bd9d63fdca6aa96ee4410bc0853773
amcs <lassanadiakite90@gmail.com>
2021-01-31 00:02:11 +0100

    La finalisation du déploiement graphql 1.0.2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require ('@babel/polyfill');
+require ('@babel/polyfill')
 const {GraphQLServer} = require('graphql-yoga');
 const {Prisma} = require('prisma-binding');
 const resolvers = require('./resolvers');
