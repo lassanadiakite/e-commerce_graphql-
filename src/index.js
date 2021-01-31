43dc02cb52ee028f126b0fcc79e90cc4dd7e237b
amcs <lassanadiakite90@gmail.com>
2021-01-31 22:37:08 +0100

    Le deploiement 1.0.5

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '@babel/polyfill'
+require('@babel/polyfill');
 const {GraphQLServer} = require('graphql-yoga');
 const {Prisma} = require('prisma-binding');
 const resolvers = require('./resolvers');
